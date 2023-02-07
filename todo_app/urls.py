b3a371c21c13fee11f17382a2dfe1df080b7c5e7
minwocee <minwocee@naver.com>
2023-02-07 17:11:24 +0900

    3번쨰 커밋

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     # 이 부부분을 채울 예정이다.
-    path('',views.index),
+    path('',views.Todos),
 ]
 
--- todo_app/views.py
+++ todo_app/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from .models import Todo
 # Create your views here.
 
-def index(request):
+def Todos(request):
     #필터를 추가한다.(내림차순으로 정렬한다.)
     todos = Todo.objects.all().order_by('-pk')
 
     return render(
         request, 
-        'todo_app/index.html',
+        'todo_app/todos.html',
         {   
             #todos 를 html 변수에 넣어주는 과정
             'todos' : todos,
